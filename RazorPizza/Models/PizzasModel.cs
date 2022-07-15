4d33464099f9633773522195d73450ef616298dd
phillip shields <matua.phillip.shields@gmail.com>
2022-07-15 20:59:21 +1200

    added bindings for custom pizza and checkout

---
+++ RazorPizza/Models/PizzasModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RazorPizza.Models
+{
+    public class PizzasModel
+    {
+        public string ImageTitle { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; } = 2;
+        public bool TomatoSauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Pepperoni { get; set; }
+        public bool Mushroom { get; set; }
+        public bool Pineapple { get; set; }
+        public bool Sausage { get; set; }
+        public bool GreenPeppers { get; set; }
+    }
+}
--- RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizza.Models;
 
 namespace RazorPizza.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel ThisPizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = ThisPizza.BasePrice;
+            if (ThisPizza.TomatoSauce) PizzaPrice += 1;
+            if (ThisPizza.Cheese) PizzaPrice += 1;
+            if (ThisPizza.Pepperoni) PizzaPrice += 1;
+            if (ThisPizza.Mushroom) PizzaPrice += 1;
+            if (ThisPizza.Pineapple) PizzaPrice += 1;
+            if (ThisPizza.Sausage) PizzaPrice += 1;
+            if (ThisPizza.GreenPeppers) PizzaPrice += 1;
+
+            return RedirectToPage("/CheckOut/Checkout", new { ThisPizza.PizzaName, PizzaPrice });
+            
         }
     }
 }
